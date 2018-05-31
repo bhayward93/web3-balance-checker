1dfb5ad752430223d6e64a4c0139d107a447910d
nemo <benhayward93@hotmail.com>
2018-05-31 16:49:54 +0100

    Added transaction log of the wallet address

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var Web3 = require("web3");
-let address = '0x0Ca13c198b796918196A862C521106e2BcB157c3';
+const Web3 = require("web3");
+const address = '0x0Ca13c198b796918196A862C521106e2BcB157c3';
+const request = require('request');
 
 if (typeof web3 !== 'undefined') {
 	web3 = new Web3(web3.currentProvider);
@@ -14,3 +15,14 @@ let balance =  web3.eth.getBalance(address, function (error, result) {
         console.log('Ether:', web3.utils.fromWei(result,'ether')); 
     }
 });
+
+/**
+ * Directly logs the transactions assosciated with an address in console.  
+ */
+function logTransactionHistory(addr){   
+	request('http://api-rinkeby.etherscan.io/api?module=account&action=txlist&address='+addr, { json: true }, (err, res, body) => {
+	  if (err) { return console.log(err); }
+	  console.log(body);
+	});
+}
+logTransactionHistory("0x0Ca13c198b796918196A862C521106e2BcB157c3");
