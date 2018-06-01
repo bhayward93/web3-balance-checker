d2de5dbd13ca4dac1c242f533f39708c2e50db26
nemo <benhayward93@hotmail.com>
2018-06-01 10:09:43 +0100

    New wallet creation for Ethereum and Bitcoin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Web3 = require("web3");
 const address = '0x0Ca13c198b796918196A862C521106e2BcB157c3';
 const request = require('request');
+const crypto = require('crypto');
 
 if (typeof web3 !== 'undefined') {
 	web3 = new Web3(web3.currentProvider);
@@ -24,5 +25,18 @@ function logTransactionHistory(addr){
 	  if (err) { return console.log(err); }
 	  console.log(body);
 	});
+}
+
+function genKeyPair(){
+	var crypto = require('crypto');
+	var prime_length = 60;
+	var diffHell = crypto.createDiffieHellman(prime_length);
+
+	diffHell.generateKeys('base64');
+	console.log("Public Key : " ,diffHell.getPublicKey('base64'));
+	console.log("Private Key : " ,diffHell.getPrivateKey('base64'));
+
+	console.log("Public Key : " ,diffHell.getPublicKey('hex'));
+	console.log("Private Key : " ,diffHell.getPrivateKey('hex'));
 }
 logTransactionHistory("0x0Ca13c198b796918196A862C521106e2BcB157c3");
---
+++ newWallet.js
@@ -0,0 +1,33 @@
+const request = require('request');
+
+/**
+ *Creates a new BTC address using the BlockCypher API
+ */
+function newBTCAddress(){
+	request.post('https://api.blockcypher.com/v1/btc/test3/addrs', { json: true }, (err, res, body) => {
+		if (err) { return console.log(err); }
+		try{
+			console.log("Success! Bitcoin address created. Please be sure to save your private key in a safe location \n\n")
+	  		console.log(body);
+		} catch(e){
+	  		console.log("Something has went wrong with the APIs endpoint. Please try again later.");
+		}
+	});
+}
+
+/**
+ *Creates a new Eth address using the BlockCypher API
+ */
+function newEthAddress(){
+	request.post('https://api.blockcypher.com/v1/beth/test/addrs', { json: true }, (err, res, body) => {
+		if (err) { return console.log(err); }
+		try{
+			console.log("Success! Ethereum address created. Please be sure to save your private key in a safe location \n\n")
+	  		console.log(body);
+		} catch(e){
+	  		console.log("Something has went wrong with the APIs endpoint. Please try again later.");
+		}
+	});
+}
+newBTCAddress();
+newEthAddress();
