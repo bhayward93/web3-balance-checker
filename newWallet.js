e87d2ce912aa29e26c18e362f53eccc4de00dcb2
nemo <benhayward93@hotmail.com>
2018-06-01 10:11:42 +0100

    formatting stuff

--- newWallet.js
+++ newWallet.js
@@ -7,7 +7,7 @@ function newBTCAddress(){
 	request.post('https://api.blockcypher.com/v1/btc/test3/addrs', { json: true }, (err, res, body) => {
 		if (err) { return console.log(err); }
 		try{
-			console.log("Success! Bitcoin address created. Please be sure to save your private key in a safe location \n\n")
+			console.log("Success! Bitcoin address created. Please be sure to save your private key in a safe location. \n\n")
 	  		console.log(body);
 		} catch(e){
 	  		console.log("Something has went wrong with the APIs endpoint. Please try again later.");
@@ -22,7 +22,7 @@ function newEthAddress(){
 	request.post('https://api.blockcypher.com/v1/beth/test/addrs', { json: true }, (err, res, body) => {
 		if (err) { return console.log(err); }
 		try{
-			console.log("Success! Ethereum address created. Please be sure to save your private key in a safe location \n\n")
+			console.log("\n\n-----\n\nSuccess! Ethereum address created. Please be sure to save your private key in a safe location. \n\n")
 	  		console.log(body);
 		} catch(e){
 	  		console.log("Something has went wrong with the APIs endpoint. Please try again later.");
